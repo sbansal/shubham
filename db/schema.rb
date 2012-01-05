e103bc5a3d8783e265b3ab19ae381f99e52e7df4
sbansal <shubhambansal15@gmail.com>
2012-01-05 14:13:03 -0600

    Added admin roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120102032335) do
+ActiveRecord::Schema.define(:version => 20120105174607) do
 
   create_table "basketizations", :force => true do |t|
     t.integer  "task_id"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(:version => 20120102032335) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "fullname"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
