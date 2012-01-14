ab8201f074f1e459e7e40ff9c642e792a5cd6f7d
sbansal <shubhambansal15@gmail.com>
2012-01-14 15:24:57 -0600

    hardcoded aws properties to work with the assets precompiling

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,8 +1,8 @@
 AssetSync.configure do |config|
   config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  config.fog_directory = ENV['FOG_DIRECTORY']
+  config.aws_access_key_id = 'AKIAJ6VMTPKJOGWKTHAA'
+  config.aws_secret_access_key = 'kZ2bH7vIQ3MbqYeh0vuoi33MYqzSG5gMcw+8GYXk'
+  config.fog_directory = 'tracely-assets'
   
   # Increase upload performance by configuring your region
   # config.fog_region = 'eu-west-1'
